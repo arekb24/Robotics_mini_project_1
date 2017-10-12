38e2d831d932da8fdb337d74464baf297b829f8f
Unknown <s142466@student.dtu.dk>
2017-10-12 18:31:10 +0200

    Auto stash before merge of "master" and "origin/master"

--- Build1/src/pathplanning.cpp
+++ Build1/src/pathplanning.cpp
@@ -39,19 +39,23 @@ bool checkCollisions(Device::Ptr device, const State &state, const CollisionDete
 }
 
 int main(int argc, char** argv) {
-	const string wcFile = "../../Kr16WorkCell/Scene.wc.xml";
+	const string wcFile = "../../Kr16WallWorkCell/Scene.wc.xml";
 	const string deviceName = "KukaKr16";
 	cout << "Trying to use workcell " << wcFile << " and device " << deviceName << endl;
 
 	WorkCell::Ptr wc = WorkCellLoader::Factory::load(wcFile);
+
 	Device::Ptr device = wc->findDevice(deviceName);
+
 	if (device == NULL) {
 		cerr << "Device: " << deviceName << " not found!" << endl;
 		return 0;
 	}
+
 	const State state = wc->getDefaultState();
 
 	CollisionDetector detector(wc, ProximityStrategyFactory::makeDefaultCollisionStrategy());
+
 	PlannerConstraint constraint = PlannerConstraint::make(&detector,device,state);
 
 	/** Most easy way: uses default parameters based on given device
